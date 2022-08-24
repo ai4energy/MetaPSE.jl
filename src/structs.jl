6b43f8a984ddcc7f653751aa10219ebba5f96ef1
mingtaoli <mingtao@xjtu.edu.cn>
2022-08-24 21:02:38 +0800

    琢磨变量的形式

--- src/structs.jl
+++ src/structs.jl
@@ -3,10 +3,14 @@ using Unitful
 
 Base.@kwdef struct PSEVariable
     name::Symbol
-    unit::Unitful.FreeUnits
-    defaultValue::Float64
-    MinValue::Float64
-    MaxValue::Float64
+    unit::Unitful.FreeUnits #就是只能使用我们定义好的那些unit，如何来check？
+    defaultvalue::Float64
+    lowerbound::Float64
+    upperbound::Float64
+    initialgues::Float64
+    #constraint要不要？
+    #mktvarsym
 end
 
-abc=PSEVariable(:abc,PSEUnits.kilomole_per_cubic_metre,1.0, 0.0, 5.0)
+abc=PSEVariable(:abc,PSEUnits.kilomole_per_cubic_metre,1.0, 0.0, 5.0, 2.0)
+
